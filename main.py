eb796f3d6e038e8883a561fe2844c94b26b4f3ce
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-18 16:30:03 -0500

    interaccion comunicativa unaria

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("cls")
+print("GESTION DE TRABAJADORES")
+print("-----------------------")
+print("Opciones:")
+
+opcion = int(input("Ingrese una opción vía su código numérico: "))
