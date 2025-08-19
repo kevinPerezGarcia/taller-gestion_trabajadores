c3abf06d759534a83fcee793b7bf0e5d069eaa86
kevinPerezGarcia <econ.perez.garcia.k@gmail.com>
2025-08-18 21:14:54 -0500

    interaccion registrar -> interaccion comunicativa unaria

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ dct_trabajador = {
     "anio_ingreso": "",
     "salario": ""    
 }
+opciones = [0, 1]
 
 while True:
     os.system("cls")
@@ -16,14 +17,20 @@ while True:
     print("-----------------------")
     print("Opciones:")
     print("0. Salir")
+    print("1. Registrar trabajador")
 
     while True:
         opcion = int(input("Ingrese una opción vía su código numérico: "))
         try:
-            assert (opcion in [0]) is True
+            assert (opcion in opciones) is True
             break
         except AssertionError:
             print("¡Opción numérica incorrecta!. Vuelva a intentarlo.")
 
     if opcion == 0:
+        print("¡Usted salió del programa!")
         break
+    else:
+        os.system("cls")
+        print("REGISTRO DE TRABAJADORES")
+        print("------------------------")
