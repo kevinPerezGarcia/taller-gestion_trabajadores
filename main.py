496edd8fa8465aca0f96d23f4b7170eefaaeba12
kevinPerezGarcia <econ.perez.garcia.k@gmail.com>
2025-08-18 21:50:35 -0500

    interaccion operativo "registrar" arbitrario

--- main.py
+++ main.py
@@ -13,30 +13,43 @@ opciones = [0, 1]
 
 while True:
     os.system("cls")
-    print("GESTION DE TRABAJADORES")
-    print("-----------------------")
-    print("Opciones:")
-    print("0. Salir")
-    print("1. Registrar trabajador")
+    print("################################")
+    print("SISTEMA. GESTION DE TRABAJADORES")
+    print("################################")
 
-    while True:
-        opcion = int(input("Ingrese una opción vía su código numérico: "))
-        try:
-            assert (opcion in opciones) is True
-            break
-        except AssertionError:
-            print("¡Opción numérica incorrecta!. Vuelva a intentarlo.")
+    print("\nMenú de Opciones:\n")
+    print("0. Salir")
+    print("1. Componente. Registrar trabajador")
+
+    opcion = int(input("\nIngrese una opción vía su código numérico: "))
 
     if opcion == 0:
         os.system("cls")
-        print("¡Usted salió del programa!")
+        print("¡Usted salió del programa!\n")
         break
     else:
         while True:
             os.system("cls")
-            print("REGISTRO DE TRABAJADORES")
-            print("------------------------")
-            opcion = input("Ingrese 000 para volver al menú de opciones: ")
-            if opcion == '000':
+            print("====================================")
+            print("COMPONENTE. REGISTRO DE TRABAJADORES")
+            print("====================================")
+
+            print("\nOpciones:\n")
+            print("0. Volver")
+            print("1. Acción. Registrar trabajador")
+
+            opcion = int(input("\nIngrese una opción vía su código numérico: "))
+            if opcion == 0:
                 break
-            
+            else:
+                print("\nIngrese la siguiente información del trabajador a registrar:\n")
+                dct_trabajador['dni'] = input("DNI: ")
+                dct_trabajador['nombres_apellidos'] = input("Nombres y apellidos: ")
+                dct_trabajador['sexo'] = input("Sexo: ")
+                dct_trabajador['edad'] = input("Edad: ")
+                dct_trabajador['anio_ingreso'] = input("Año de ingreso: ")
+                dct_trabajador['salario'] = input("Salario: ")
+
+                lst_trabajadores.append(dct_trabajador.copy())
+
+                input("\n¡Trabajador registrado! Presione cualquier tecla para continuar.")
