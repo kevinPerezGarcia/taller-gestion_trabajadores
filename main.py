d03caa8224f95ebcb1059db451c83870271317c3
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-19 12:59:09 -0500

    interaccion operativa comunicativo "consultar"

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ while True:
     print("0. Salir")
     print("1. Componente. Registrar trabajador")
     print("2. Componente. Listar trabajadores")
+    print("3. Componente. Consultar trabajador")
 
     opcion = int(input("\nIngrese una opción vía su código numérico: "))
 
@@ -81,4 +82,44 @@ while True:
                     print(f"{trabajador["edad"]:<6}", end=" ")
                     print(f"{trabajador["anio_ingreso"]:<14}", end=" ")
                     print(f"{trabajador["salario"]:<7}")
+                input("\nPulse la tecla 'ENTER' para continuar.")
+    
+    elif opcion == 3:
+        while True:
+            os.system("cls")
+            print("==================================")
+            print("COMPONENTE. CONSULTA DE TRABAJADOR")
+            print("==================================")
+            print("\nOpciones:")
+            print("0. Salir")
+            print("1. Acción. Consultar trabajador")
+
+            opcion = int(input("\nIngrese una opción vía su código numérico: "))
+            if opcion == 0:
+                break
+            elif opcion == 1:
+                os.system("cls")
+                print("----------------------------")
+                print("ACCION. CONSULTAR TRABAJADOR")
+                print("----------------------------")
+                dni_a_buscar = input("\nIngrese el DNI del trabajador a consultar: ")
+
+                for ind, trabajador in enumerate(lst_trabajadores):
+                    if trabajador["dni"] == dni_a_buscar:
+                        tpl_consulta = (True, ind, trabajador)
+                    else:
+                        tpl_consulta = (False, None)
+
+                if tpl_consulta[0]:
+                    dct_trabajador_consultado = tpl_consulta[2]
+                    print("")
+                    print(f"DNI:                 {dct_trabajador_consultado["dni"]}")
+                    print(f"Nombres y apellidos: {dct_trabajador_consultado["nombres_apellidos"]}")
+                    print(f"Sexo:                {dct_trabajador_consultado["sexo"]}")
+                    print(f"Edad:                {dct_trabajador_consultado["edad"]}")
+                    print(f"Año de ingreso:      {dct_trabajador_consultado["anio_ingreso"]}")
+                    print(f"Salario:             {dct_trabajador_consultado["salario"]}")
+                else:
+                    print(f"\n¡Trabajador, con DNI: {dni_a_buscar}, no registrado!")
+                
                 input("\nPulse la tecla 'ENTER' para continuar.")
