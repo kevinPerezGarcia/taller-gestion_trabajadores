8430c3e2364181c55bd96587dbdd7e957b409737
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-19 09:52:36 -0500

    interaccion operativo comunicativo "listar"

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ dct_trabajador = {
     "anio_ingreso": "",
     "salario": ""    
 }
-opciones = [0, 1]
 
 while True:
     os.system("cls")
@@ -20,6 +19,7 @@ while True:
     print("\nMenú de Opciones:\n")
     print("0. Salir")
     print("1. Componente. Registrar trabajador")
+    print("2. Componente. Listar trabajadores")
 
     opcion = int(input("\nIngrese una opción vía su código numérico: "))
 
@@ -27,7 +27,7 @@ while True:
         os.system("cls")
         print("¡Usted salió del programa!\n")
         break
-    else:
+    elif opcion == 1:
         while True:
             os.system("cls")
             print("====================================")
@@ -53,3 +53,32 @@ while True:
                 lst_trabajadores.append(dct_trabajador.copy())
 
                 input("\n¡Trabajador registrado! Presione cualquier tecla para continuar.")
+    elif opcion == 2:
+        while True:
+            os.system("cls")
+            print("=================================")
+            print("COMPONENTE. LISTA DE TRABAJADORES")
+            print("=================================")
+            print("\nOpciones:")
+            print("0. Volver")
+            print("1. Acción. Listar trabajadores")
+            
+            opcion = int(input("\nIngrese una opción vía su código numérico: "))
+
+            if opcion == 0:
+                break
+            elif opcion == 1:
+                os.system("cls")
+                print("---------------------")
+                print("LISTA DE TRABAJADORES")
+                print("---------------------\n")
+                print("DNI    Nombres y Apellidos Sexo   Edad   Año de Ingreso Salario")
+                print("...... ................... ...... ...... .............. .......")
+                for trabajador in lst_trabajadores:
+                    print(f"{trabajador["dni"]:<6}", end=" ")
+                    print(f"{trabajador["nombres_apellidos"]:<19}", end=" ")
+                    print(f"{trabajador["sexo"]:<6}", end= " ")
+                    print(f"{trabajador["edad"]:<6}", end=" ")
+                    print(f"{trabajador["anio_ingreso"]:<14}", end=" ")
+                    print(f"{trabajador["salario"]:<7}")
+                input("\nPulse la tecla 'ENTER' para continuar.")
