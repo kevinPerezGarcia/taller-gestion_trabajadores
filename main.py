febd680a5b63a8d4f2cdf9210719ef43567bc4e3
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-20 18:26:42 -0500

    funcion consultar trabajador

--- main.py
+++ main.py
@@ -90,6 +90,47 @@ def listar_trabajadores() -> None:
             input("\nPulse la tecla 'ENTER' para continuar.")
 
 
+def consultar_trabajador() -> None:
+    while True:
+        limpiar_consola()
+        print("==================================")
+        print("COMPONENTE. CONSULTA DE TRABAJADOR")
+        print("==================================")
+        print("\nOpciones:\n")
+        print("0. Salir")
+        print("1. Acción. Consultar trabajador")
+
+        opcion = int(input("\nIngrese una opción vía su código numérico: "))
+        if opcion == 0:
+            break
+        elif opcion == 1:
+            limpiar_consola()
+            print("----------------------------")
+            print("ACCION. CONSULTAR TRABAJADOR")
+            print("----------------------------")
+            dni_a_buscar = input("\nIngrese el DNI del trabajador a consultar: ")
+
+            for ind, trabajador in enumerate(lst_trabajadores):
+                if trabajador["dni"] == dni_a_buscar:
+                    tpl_consulta = (True, ind, trabajador)
+                else:
+                    tpl_consulta = (False, None)
+
+            if tpl_consulta[0]:
+                dct_trabajador_consultado = tpl_consulta[2]
+                print("")
+                print(f"DNI:                 {dct_trabajador_consultado["dni"]}")
+                print(f"Nombres y apellidos: {dct_trabajador_consultado["nombres_apellidos"]}")
+                print(f"Sexo:                {dct_trabajador_consultado["sexo"]}")
+                print(f"Edad:                {dct_trabajador_consultado["edad"]}")
+                print(f"Año de ingreso:      {dct_trabajador_consultado["anio_ingreso"]}")
+                print(f"Salario:             {dct_trabajador_consultado["salario"]}")
+            else:
+                print(f"\n¡Trabajador, con DNI: {dni_a_buscar}, no registrado!")
+            
+            input("\nPulse la tecla 'ENTER' para continuar.")
+
+
 while True:
     opcion = interfaz()
     if opcion == 0:
@@ -99,44 +140,7 @@ while True:
     elif opcion == 2:
         listar_trabajadores()
     elif opcion == 3:
-        while True:
-            limpiar_consola()
-            print("==================================")
-            print("COMPONENTE. CONSULTA DE TRABAJADOR")
-            print("==================================")
-            print("\nOpciones:\n")
-            print("0. Salir")
-            print("1. Acción. Consultar trabajador")
-
-            opcion = int(input("\nIngrese una opción vía su código numérico: "))
-            if opcion == 0:
-                break
-            elif opcion == 1:
-                limpiar_consola()
-                print("----------------------------")
-                print("ACCION. CONSULTAR TRABAJADOR")
-                print("----------------------------")
-                dni_a_buscar = input("\nIngrese el DNI del trabajador a consultar: ")
-
-                for ind, trabajador in enumerate(lst_trabajadores):
-                    if trabajador["dni"] == dni_a_buscar:
-                        tpl_consulta = (True, ind, trabajador)
-                    else:
-                        tpl_consulta = (False, None)
-
-                if tpl_consulta[0]:
-                    dct_trabajador_consultado = tpl_consulta[2]
-                    print("")
-                    print(f"DNI:                 {dct_trabajador_consultado["dni"]}")
-                    print(f"Nombres y apellidos: {dct_trabajador_consultado["nombres_apellidos"]}")
-                    print(f"Sexo:                {dct_trabajador_consultado["sexo"]}")
-                    print(f"Edad:                {dct_trabajador_consultado["edad"]}")
-                    print(f"Año de ingreso:      {dct_trabajador_consultado["anio_ingreso"]}")
-                    print(f"Salario:             {dct_trabajador_consultado["salario"]}")
-                else:
-                    print(f"\n¡Trabajador, con DNI: {dni_a_buscar}, no registrado!")
-                
-                input("\nPulse la tecla 'ENTER' para continuar.")
+        consultar_trabajador()
     elif opcion == 4:
         while True:
             limpiar_consola()
