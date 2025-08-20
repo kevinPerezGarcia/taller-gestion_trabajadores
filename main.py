8ee050ea225b711b813e382c325901a8e2d668b3
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-20 18:30:53 -0500

    funcion principal "main"

--- main.py
+++ main.py
@@ -217,17 +217,21 @@ def modificar_trabajador() -> None:
                 print(f"\n¡Trabajador, con DNI: {dni_a_modificar}, no registrado!")
 
 
-while True:
-    opcion = interfaz()
-    if opcion == 0:
-        break
-    elif opcion == 1:
-        registrar_trabajador()
-    elif opcion == 2:
-        listar_trabajadores()
-    elif opcion == 3:
-        consultar_trabajador()
-    elif opcion == 4:
-        eliminar_trabajador()
-    elif opcion == 5:
-        modificar_trabajador()
+def main() -> None:
+    while True:
+        opcion = interfaz()
+        if opcion == 0:
+            break
+        elif opcion == 1:
+            registrar_trabajador()
+        elif opcion == 2:
+            listar_trabajadores()
+        elif opcion == 3:
+            consultar_trabajador()
+        elif opcion == 4:
+            eliminar_trabajador()
+        elif opcion == 5:
+            modificar_trabajador()
+
+
+main()
