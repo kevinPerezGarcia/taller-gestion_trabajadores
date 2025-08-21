0c4c7ec02e6dc0569b4a830294f8478d9e1bc975
Kevin Pérez García <econ.perez.garcia.k@gmail.com>
2025-08-21 11:04:46 -0500

    modulo listar trabajadores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from modulos.interfaz import interfaz
 from modulos.registrar import registrar_trabajador
+from modulos.listar import listar_trabajadores
 
 lst_trabajadores = []
 
@@ -11,7 +12,7 @@ def main() -> None:
         elif opcion == 1:
             registrar_trabajador(lst_trabajadores)
         elif opcion == 2:
-            listar_trabajadores()
+            listar_trabajadores(lst_trabajadores)
         elif opcion == 3:
             consultar_trabajador()
         elif opcion == 4:
--- main_monolitico.py
+++ main_monolitico.py
@@ -1,34 +1,3 @@
-def listar_trabajadores() -> None:
-    while True:
-        limpiar_consola()
-        print("=================================")
-        print("COMPONENTE. LISTA DE TRABAJADORES")
-        print("=================================")
-        print("\nOpciones:\n")
-        print("0. Volver")
-        print("1. Acción. Listar trabajadores")
-        
-        opcion = int(input("\nIngrese una opción vía su código numérico: "))
-
-        if opcion == 0:
-            break
-        elif opcion == 1:
-            limpiar_consola()
-            print("---------------------")
-            print("LISTA DE TRABAJADORES")
-            print("---------------------\n")
-            print("DNI    Nombres y Apellidos Sexo   Edad   Año de Ingreso Salario")
-            print("...... ................... ...... ...... .............. .......")
-            for trabajador in lst_trabajadores:
-                print(f"{trabajador["dni"]:<6}", end=" ")
-                print(f"{trabajador["nombres_apellidos"]:<19}", end=" ")
-                print(f"{trabajador["sexo"]:<6}", end= " ")
-                print(f"{trabajador["edad"]:<6}", end=" ")
-                print(f"{trabajador["anio_ingreso"]:<14}", end=" ")
-                print(f"{trabajador["salario"]:<7}")
-            input("\nPulse la tecla 'ENTER' para continuar.")
-
-
 def consultar_trabajador() -> None:
     while True:
         limpiar_consola()
